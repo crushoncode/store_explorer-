0b67539c14f5e3f678d58dabcaa6ae047db1567e
Serina Ko <serinako08@gmail.com>
2019-01-02 17:11:17 +1100

    modify redux file structure and add mapActionsToProps

--- src/App.js
+++ src/App.js
@@ -2,27 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+import { updateUser } from './actions/user-actions';
+
 class App extends Component {
+  onUpdateUser = () => {
+    this.props.onUpdateUser('Sammy');
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
         </header>
+        <div onClick={() => this.onUpdateUser()}>Update user</div>
+        {this.props.user}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  products: state.products,
+  user: state.user
+});
+
+const mapActionsToProps = {
+  onUpdateUser: updateUser
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(App);
+
+// With mapActionsToProps, we don't have to use dispatch
